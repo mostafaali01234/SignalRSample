bdba95b68dea7a27709e485bb6edc21139ec16f3
mostafa ali <mostafa.salem.se@gmail.com>
2025-07-10 11:41:07 +0300

    Added Hub and method for UserTotalViews

--- SignalRSample/Program.cs
+++ SignalRSample/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SignalRSample.Data;
+using SignalRSample.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSignalR();
+
 
 var app = builder.Build();
 
@@ -42,5 +46,7 @@ app.MapControllerRoute(
 
 app.MapRazorPages()
    .WithStaticAssets();
+
+app.MapHub<UserHub>("/hubs/userCount");
 
 app.Run();
