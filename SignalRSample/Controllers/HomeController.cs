2c62e34b9b3b634ca1801c118e99c5d7ab12487f
mostafa ali <mostafa.salem.se@gmail.com>
2025-07-13 16:52:20 +0300

    Seperated Chat view from index

--- SignalRSample/Controllers/HomeController.cs
+++ SignalRSample/Controllers/HomeController.cs
@@ -58,7 +58,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult BasicChat()
     {
         return View();
     }
